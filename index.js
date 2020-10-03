b40217c5c2e787a81c91736a63adc02c8c3e79d8
Kirugaming <Kiruthegreat2@gmail.com>
2020-10-03 16:52:48 -0500

    Money System

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
 const fs = require('fs');
-// require the discord.js module
 const Discord = require('discord.js');
-
 const { prefix, token } = require('./config.json');
-
-// create a new Discord client
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
-
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 
 	// set a new item in the Collection
-	// with the key as the command name and the value as the exported module
 	client.commands.set(command.name, command);
 }
 
 const cooldowns = new Discord.Collection();
 
 // when the client is ready, run this code
-// this event will only trigger one time after logging in
 client.once('ready', () => {
 	console.log('Ready!');
 });
